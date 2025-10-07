06b1ac298db95a81b8ff2af01f16fd9b14e7d797
mutua <mutua.mutinda@valuechainfactory.com>
2025-10-07 15:48:11 +0300

    add favicon

--- config/server.ts
+++ config/server.ts
@@ -1,11 +1,9 @@
 export default ({ env }) => ({
   host: env("HOST", "0.0.0.0"),
   port: env.int("PORT", 1338),
-  url: env("STRAPI_ADMIN_BACKEND_URL", "https://dev.legalsister.org"),
+  url: env("STRAPI_ADMIN_BACKEND_URL"),
   app: {
     keys: env.array("APP_KEYS"),
   },
   proxy: true,
-  // Allow your domain
-  allowedHosts: ["dev.legalsister.org", "www.dev.legalsister.org"],
 });
